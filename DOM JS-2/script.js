c5c4c310598d3fe68258bd71fc83a05a34ebd06c
Prajwal H C <68982347+prajwalhc-18@users.noreply.github.com>
2023-10-21 18:44:01 +0530

    Add files via upload

---
+++ DOM JS-2/script.js
@@ -0,0 +1,14 @@
+let colorButton = document.getElementById('colorButton')
+
+colorButton.addEventListener('click',function(){
+    document.body.style.backgroundColor = getRandomColor();
+})
+
+function getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
