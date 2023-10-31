924c3b99991f8cf26a0899bfcab2e498fbc2f195
Prajwal H C <68982347+prajwalhc-18@users.noreply.github.com>
2023-10-31 11:25:09 +0530

    Delete DOM JS-3 directory

--- DOM JS-3/script.js
+++
@@ -1,6 +0,0 @@
-let toggleButton = document.getElementById('toggleButton')
-let toggleText = document.getElementById('toggleText')
-
-toggleButton.addEventListener('click',function(){
-    toggleText.textContent = toggleText.textContent === 'Hello'?'World!':'Hello'
-})
